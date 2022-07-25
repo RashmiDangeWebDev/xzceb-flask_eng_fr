54322596896b139d67357e3c968fdc989f39377d
RashmiDangeWebDev <rashmi.dange@gmail.com>
2022-07-25 23:20:01 +0000

    Final Changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -5,11 +5,11 @@ class TestenglishToFrench(unittest.TestCase):
     def test1(self):
         self.assertEqual(english_to_french('NULL'),'NULL') #test to check null input sould give null output
         self.assertEqual(english_to_french('Hello'),'Bonjour') # test to check proper output
-      
+        self.assertNotEqual(english_to_french('Hello'),'Hello')
 
 class TestengfrenchToEnglish(unittest.TestCase):
     def test1(self):
         self.assertEqual(french_to_english('NULL'),'NULL') #test to check null input sould give null output
         self.assertEqual(french_to_english('Bonjour'),'Hello') # test to check proper output
-              
+        self.assertNotEqual(french_to_english('Bonjour'),'Bonjour')      
 unittest.main()
